f7591b780b9e48982fac284f9119c63a2e6a5f17
Wolfgang Becker <wolli.becker@gmail.com>
2018-05-20 22:09:59 +0200

    Set up user authentication with devise

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'semantic-ui-sass'
 # File uploading
 gem 'carrierwave'
+# Authentication
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Default URL options (required by Devise)
+  config.action_mailer.default_url_options = {
+    host: ENV["RAILS_HOST"],
+    port: ENV["RAILS_PORT"]
+  }
 end
