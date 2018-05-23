feab36a0901e8841b580b58af90cb89249506f6a
Wolfgang Becker <wolli.becker@gmail.com>
2018-05-23 23:41:06 +0200

    Add specs for User model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+
+  it 'should validate the email format' do
+    subject.email = Faker::Internet.email
+    subject.valid?
+    expect(subject.errors.keys).not_to include(:email)
+
+    subject.email = 'wrongformat'
+    subject.valid?
+    expect(subject.errors.keys).to include(:email)
+  end
 end
