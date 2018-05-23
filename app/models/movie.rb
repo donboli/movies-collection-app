7d6f5ca9ba2c6f605a318c3fefb9deff036c3389
Wolfgang Becker <wolli.becker@gmail.com>
2018-05-23 23:25:19 +0200

    Add validations and specs for Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,13 @@ class Movie < ApplicationRecord
   mount_uploaders :pictures, PictureUploader
 
   belongs_to :user
+  acts_as_tenant(:user)
 
   validates :name, presence: true
-
-  acts_as_tenant(:user)
+  validates :rating, presence: true,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0,
+      less_than_or_equal_to: 5
+    }
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -9,5 +9,8 @@ RSpec.describe Movie, type: :model do
     ActsAsTenant.default_tenant = nil
   end
 
-  it { is_expected.to validate_presence_of(:name) }
+  it { should validate_presence_of(:name) }
+  it { should validate_numericality_of(:rating).only_integer }
+  it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
 end
