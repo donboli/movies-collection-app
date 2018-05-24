e9fa314690e2e0a61e1157fb11341307a1d3a456
Wolfgang Becker <wolli.becker@gmail.com>
2018-05-25 01:50:48 +0200

    Clean up view specs for movies#edit and movies#new

--- spec/views/movies/edit.html.erb_spec.rb
+++ spec/views/movies/edit.html.erb_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "movies/edit", type: :view do
   before(:each) do
     ActsAsTenant.current_tenant = create(:user)
     @movie = assign(:movie, create(:movie))
+    render
   end
 
   after do
@@ -14,20 +15,14 @@ RSpec.describe "movies/edit", type: :view do
   end
 
   it "renders the edit title" do
-    render
-
     assert_select 'h1', 'Editing Movie'
   end
 
   it "renders a link to movies#index" do
-    render
-
     assert_select "a[href='/movies']"
   end
 
   it "renders a link to movies#show" do
-    render
-
     assert_select "a[href='/movies/#{@movie.id}']"
   end
 end
--- spec/views/movies/new.html.erb_spec.rb
+++ spec/views/movies/new.html.erb_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "movies/new", type: :view do
   before(:each) do
     ActsAsTenant.current_tenant = create(:user)
     @movie = assign(:movie, create(:movie))
+    render
   end
 
   after do
@@ -14,14 +15,10 @@ RSpec.describe "movies/new", type: :view do
   end
 
   it "renders the new title" do
-    render
-
     assert_select 'h1', 'New Movie'
   end
 
   it "renders a link to movies#index" do
-    render
-
     assert_select "a[href='/movies']"
   end
 end
