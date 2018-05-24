e3464fed56d13b27f166cd30cc626a769889533b
Wolfgang Becker <wolli.becker@gmail.com>
2018-05-25 01:14:04 +0200

    Add view specs for movies#index

--- spec/views/movies/index.html.erb_spec.rb
+++ spec/views/movies/index.html.erb_spec.rb
@@ -1,31 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe "movies/index", type: :view do
-  before(:each) do
-    assign(:movies, [
-      Movie.create!(
-        :name => "Name",
-        :synopsis => "MyText",
-        :rating => 2,
-        :actors => "Actors",
-        :director => "Director"
-      ),
-      Movie.create!(
-        :name => "Name",
-        :synopsis => "MyText",
-        :rating => 2,
-        :actors => "Actors",
-        :director => "Director"
-      )
-    ])
+  before do
+    ActsAsTenant.current_tenant = create(:user)
+    @movies = assign(:movies, [create(:movie), create(:movie)])
+    render
   end
 
-  it "renders a list of movies" do
-    render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => "Actors".to_s, :count => 2
-    assert_select "tr>td", :text => "Director".to_s, :count => 2
+  after do
+    ActsAsTenant.current_tenant = nil
+  end
+
+  it "renders a card per movie" do
+    assert_select '.card', 2
+  end
+
+  it "renders movies as cards" do
+    @movies.each do |movie|
+      expect(rendered).to match movie.name
+      expect(rendered).to match movie.synopsis
+      expect(rendered).to match movie.actors
+    end
+  end
+
+  it "renders movie rating as stars" do
+    assert_select '.card' do |cards|
+      cards.each_with_index do |card, index|
+        assert_select card, '.star.active', @movies[index].rating
+        assert_select card, '.star.outline', 5 - @movies[index].rating
+      end
+    end
+  end
+
+  describe 'card buttons' do
+    it 'renders a show link' do
+      assert_select '.card' do |cards|
+        cards.each_with_index do |card, index|
+          assert_select card, "a[href='#{movie_path(@movies[index])}'][data-method='get']", 1
+        end
+      end
+    end
+
+    it 'renders an edit link' do
+      assert_select '.card' do |cards|
+        cards.each_with_index do |card, index|
+          assert_select card, "a[href='#{edit_movie_path(@movies[index])}'][data-method='get']", 1
+        end
+      end
+    end
+
+    it 'renders a delete link' do
+      assert_select '.card' do |cards|
+        cards.each_with_index do |card, index|
+          assert_select card, "a[href='#{movie_path(@movies[index])}'][data-method='delete']", 1
+        end
+      end
+    end
   end
 end
